16c5a6edd4381831f0d60528dc06e0637488de46
DAM1 <mario.ganfer@educa.jcyl.es>
2025-01-13 13:22:34 +0100

    falta la herencia de de developer con technologies

--- models/developer.py
+++ models/developer.py
@@ -2,10 +2,10 @@ from odoo import models, fields, api
 
 
 class developer(models.Model):
-    _name= 'res.pathner'
-    _inherit='res.parthner'
+    _name= 'res.partner'
+    _inherit='res.partner'
 
-    technologies = fields.Many2many(comodel_name="managemario.technology",
-                             relation="developer_technologies",
-                             column1="developer_id",
-                             column2="technologies_id")
+    technologies = fields.Many2many('managemario.technology',
+                                     relation="developer_technologies",
+                                     column1="developer_id",
+                                     column2="technologies _id")
