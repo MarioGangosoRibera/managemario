fea56504771d23503e6155c078af2e1603d8432f
DAM1 <mario.ganfer@educa.jcyl.es>
2025-01-20 12:07:54 +0100

    añadida ampliacion de comentarios

--- models/comment.py
+++ models/comment.py
@@ -6,6 +6,7 @@ class Comment(models.Model):
 
     name = fields.Char(string='Título', required=True)
     content = fields.Text(string='Contenido', required=True)
+
     task_id = fields.Many2one('managemario.task', string='Tarea relacionada', ondelete='cascade')
     project_id = fields.Many2one('managemario.project', string='Proyecto relacionado', ondelete='cascade')
     created_at = fields.Datetime(string='Fecha de creación', default=fields.Datetime.now)
